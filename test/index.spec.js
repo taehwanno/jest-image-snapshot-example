27e9484152f974667a12f3ca136bcd926749e89f
Taehwan, No <taehwanno.dev@gmail.com>
2017-08-19 16:24:08 +0900

    Relocate code related to closing browser

--- test/index.spec.js
+++ test/index.spec.js
@@ -10,8 +10,7 @@ it('Visual regression test', async () => {
   const page = await browser.newPage();
   await page.goto('http://localhost:3000');
   const screenshot = await page.screenshot();
+  browser.close();
 
   expect(screenshot).toMatchImageSnapshot(IMAGE_SNAPSHOT_CONFIG);
-
-  browser.close();
 });
